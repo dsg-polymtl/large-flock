deba7116668abfd37def21c5d6aff57664577801
Anas Dorbani <anasdorbani@gmail.com>
2024-08-24 16:34:04 +0100

    remove unused comment

--- src/include/large_flock/common.hpp
+++ src/include/large_flock/common.hpp
@@ -6,5 +6,5 @@
 #include "duckdb/function/scalar_function.hpp"
 #include "duckdb/main/extension_util.hpp"
 #include "duckdb/parser/parsed_data/create_scalar_function_info.hpp"
-// TODO doc util
+
 using namespace duckdb;
