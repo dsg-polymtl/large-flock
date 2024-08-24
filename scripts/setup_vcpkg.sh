f2138ea908be01163717183577d7e39fc313f1c7
Anas Dorbani <anasdorbani@gmail.com>
2024-08-24 20:09:32 +0100

    update the setup_vcpkg and remove the tests

--- scripts/setup_vcpkg.sh
+++ scripts/setup_vcpkg.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Step 1: Clone the vcpkg repository
-echo "Cloning the vcpkg repository..."
-git clone https://github.com/Microsoft/vcpkg.git
+# Step 1: Check if the vcpkg repository is already cloned
+if [ -d "vcpkg" ]; then
+    echo "vcpkg repository already cloned. Skipping the cloning phase..."
+else
+    echo "Cloning the vcpkg repository..."
+    git clone https://github.com/Microsoft/vcpkg.git
 
-# Check if cloning was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to clone the vcpkg repository."
-    exit 1
+    # Check if cloning was successful
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to clone the vcpkg repository."
+        exit 1
+    fi
+    echo "vcpkg repository cloned successfully."
 fi
-echo "vcpkg repository cloned successfully."
 
 # Step 2: Bootstrap vcpkg
 echo "Bootstrapping vcpkg..."
@@ -24,6 +28,4 @@ echo "vcpkg bootstrapped successfully."
 
 # Step 3: Export the VCPKG toolchain path environment variable
 export VCPKG_TOOLCHAIN_PATH=`pwd`/vcpkg/scripts/buildsystems/vcpkg.cmake
-echo "VCPKG_TOOLCHAIN_PATH is set to $VCPKG_TOOLCHAIN_PATH"
-
-echo "vcpkg setup complete."
+echo "VCPKG_TOOLCHAIN_PATH is set to 
