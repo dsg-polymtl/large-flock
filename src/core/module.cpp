29279c426a32904f65aaebe55e942c510dcf831f
Anas Dorbani <anasdorbani@gmail.com>
2024-09-03 00:34:12 +0100

    config the model and the prompt tables when extension loads

--- src/core/module.cpp
+++ src/core/module.cpp
@@ -1,14 +1,15 @@
-
 #include "large_flock/core/module.hpp"
 
 #include "large_flock/common.hpp"
 #include "large_flock/core/functions/scalar.hpp"
+#include "large_flock/core/config/lf_config.hpp"
 
 namespace large_flock {
 namespace core {
 
 void CoreModule::Register(DatabaseInstance &db) {
     CoreScalarFunctions::Register(db);
+	LfConfig::Configure(db);
 }
 
 } // namespace core
