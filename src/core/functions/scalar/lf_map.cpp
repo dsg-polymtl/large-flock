df5ea3512c6687b88b80c0e5fa13cf2130010741
Anas Dorbani <anasdorbani@gmail.com>
2024-09-13 20:26:47 +0100

    Implement Batch Execution for lf_map Based on Model Context Window

--- src/core/functions/scalar/lf_map.cpp
+++ src/core/functions/scalar/lf_map.cpp
@@ -1,20 +1,85 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
 #include <inja/inja.hpp>
+#include <iostream>
 #include <large_flock/common.hpp>
 #include <large_flock/core/functions/scalar.hpp>
 #include <large_flock/core/model_manager/model_manager.hpp>
+#include <large_flock/core/model_manager/openai.hpp>
 #include <large_flock/core/parser/llm_response.hpp>
 #include <large_flock/core/parser/scalar.hpp>
 #include <large_flock_extension.hpp>
 #include <nlohmann/json.hpp>
+#include <sstream>
+#include <string>
 
 namespace large_flock {
 namespace core {
 
-inline std::string ConstructPrompt(DataChunk &args, Connection &con) {
+inline int GetNumTokens(const std::string &str) {
+    nlohmann::json request_payload = {{"model", "text-embedding-3-small"}, {"input", str}};
+    auto num_tokens = openai::embedding().create(request_payload)["usage"]["prompt_tokens"];
+    return num_tokens;
+}
+
+template <typename T>
+std::string to_string(const T &value) {
+    std::ostringstream oss;
+    oss << value;
+    return oss.str();
+}
+
+nlohmann::json get_max_length_values(const std::vector<nlohmann::json> &params) {
+    nlohmann::json result;
+
+    for (const auto &json_obj : params) {
+        for (const auto &item : json_obj.items()) {
+            std::string key = item.key();
+            std::string value_str = to_string(item.value());
+            int length = value_str.length();
+
+            if (result.contains(key)) {
+                std::string current_max_value_str = to_string(result[key]);
+                if (current_max_value_str.length() < length) {
+                    result[key] = item.value();
+                }
+            } else {
+                result[key] = item.value();
+            }
+        }
+    }
+
+    return result;
+}
+
+std::string read_file_to_string(const std::string &file_path) {
+    std::ifstream file(file_path);
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open the file: " + file_path);
+    }
+
+    std::ostringstream content_stream;
+    content_stream << file.rdbuf();
+    return content_stream.str();
+}
+
+std::string combine_values(const nlohmann::json &json_obj) {
+    std::string combined;
+    for (const auto &item : json_obj.items()) {
+        combined += to_string(item.value()) + " ";
+    }
+
+    if (!combined.empty()) {
+        combined.pop_back();
+    }
+    return combined;
+}
+
+inline std::vector<std::string> ConstructPrompts(DataChunk &args, Connection &con, int model_max_tokens = 4096) {
     inja::Environment env;
 
     auto prompt_name = args.data[0].GetValue(0).ToString();
-
     auto query_result = con.Query(
         "SELECT prompt FROM lf_config.LARGE_FLOCK_PROMPT_INTERNAL_TABLE WHERE prompt_name = '" + prompt_name + "'");
 
@@ -23,39 +88,68 @@ inline std::string ConstructPrompt(DataChunk &args, Connection &con) {
     }
 
     auto template_str = query_result->GetValue(0, 0).ToString();
+    auto row_tokens = GetNumTokens(template_str);
 
-    std::string filled_template = "";
     auto params = CoreScalarParsers::Struct2Json(args.data[2], args.size());
-    for (idx_t i = 0; i < args.size(); i++) {
-        filled_template += "Prompt " + std::to_string(i + 1) + ": " + env.render(template_str, params[i]) + "\n\n";
+    auto max_length_values = get_max_length_values(params);
+    auto combined_values = combine_values(max_length_values);
+    row_tokens += GetNumTokens(combined_values);
+
+    std::vector<std::string> prompts;
+
+    if (row_tokens > model_max_tokens) {
+        throw std::runtime_error("The total number of tokens in the prompt exceeds the model's maximum token limit");
+    } else {
+        auto template_tokens = GetNumTokens(read_file_to_string("src/templates/lf_map/prompt_template.txt"));
+        auto max_chunk_size = (model_max_tokens - template_tokens) / row_tokens;
+        auto chunk_size = std::min(max_chunk_size, static_cast<int>(args.size()));
+        auto num_chunks = static_cast<int>(std::floor(static_cast<double>((chunk_size * row_tokens * args.size()) /
+                                                                          (model_max_tokens - template_tokens)))) +
+                          1;
+
+        for (int i = 0; i < num_chunks; ++i) {
+            std::string filled_template;
+            for (idx_t j = 0; j < chunk_size; ++j) {
+                filled_template +=
+                    "Prompt " + std::to_string(i + j + 1) + ": " + env.render(template_str, params[i + j]) + "\n\n";
+            }
+
+            std::string prompt =
+                env.render_file("src/templates/lf_map/prompt_template.txt", {{"prompts", filled_template}});
+            prompts.push_back(prompt);
+        }
     }
 
-    std::string prompt = env.render_file("src/templates/lf_map/prompt_template.txt", {{"prompts", filled_template}});
-
-    return prompt;
+    return prompts;
 }
 
 static void LfMapScalarFunction(DataChunk &args, ExpressionState &state, Vector &result) {
-    // parse the arguments and return the args key idx pair
     Connection con(*state.GetContext().db);
     CoreScalarParsers::LfMapScalarParser(args);
+
     auto model = args.data[1].GetValue(0).ToString();
-    auto prompt = ConstructPrompt(args, con);
+    auto query_result = con.Query(
+        "SELECT model, max_tokens FROM lf_config.LARGE_FLOCK_MODEL_INTERNAL_TABLE WHERE model_name = '" + model + "'");
+
+    if (query_result->RowCount() == 0) {
+        throw std::runtime_error("Model not found");
+    }
+
+    auto model_name = query_result->GetValue(0, 0).ToString();
+    auto model_max_tokens = query_result->GetValue(1, 0).GetValue<int32_t>();
+
+    auto prompts = ConstructPrompts(args, con, model_max_tokens);
+
     nlohmann::json settings;
     if (args.ColumnCount() == 4) {
         settings = CoreScalarParsers::Struct2Json(args.data[3], args.size())[0];
     }
 
-    auto query_result =
-        con.Query("SELECT model FROM lf_config.LARGE_FLOCK_MODEL_INTERNAL_TABLE WHERE model_name = '" + model + "'");
-    if (query_result->RowCount() == 0) {
-        throw std::runtime_error("Model not found");
+    nlohmann::json params;
+    for (const auto &prompt : prompts) {
+        params.update(ModelManager::CallComplete(prompt, model_name, settings));
     }
-    auto model_name = query_result->GetValue(0, 0).ToString();
 
-    auto params = ModelManager::CallComplete(prompt, model_name, settings);
-
-    // set the result
     auto index = 0;
     Vector vec(LogicalType::VARCHAR, args.size());
     UnaryExecutor::Execute<string_t, string_t>(vec, result, args.size(), [&](string_t _) {
@@ -63,9 +157,6 @@ static void LfMapScalarFunction(DataChunk &args, ExpressionState &state, Vector 
     });
 }
 
-//------------------------------------------------------------------------------
-// Register functions
-//------------------------------------------------------------------------------
 void CoreScalarFunctions::RegisterLfMapScalarFunction(DatabaseInstance &db) {
     ExtensionUtil::RegisterFunction(db, ScalarFunction("lf_map", {}, LogicalType::VARCHAR, LfMapScalarFunction, nullptr,
                                                        nullptr, nullptr, nullptr, LogicalType::ANY));
