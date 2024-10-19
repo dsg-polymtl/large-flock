5312dd2216d026e7ada67684544fdf17c5c70543
Anas Dorbani <anasdorbani@gmail.com>
2024-10-19 18:10:21 +0100

    change the GetNumTokens logic

--- src/core/model_manager/tiktoken.cpp
+++ src/core/model_manager/tiktoken.cpp
@@ -1,30 +1,16 @@
 #include <filesystem>
 #include <large_flock/core/model_manager/tiktoken.hpp>
+#include <regex>
 
 namespace large_flock {
 
 namespace core {
 
 int Tiktoken::GetNumTokens(const std::string &str) {
-
-    int length = 0;
-    std::string word;
-    for (char c : str) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                length++;
-                word.clear();
-            }
-        } else {
-            word += c;
-        }
-    }
-
-    if (!word.empty()) {
-        length++;
-    }
-
-    return length;
+    std::regex word_regex(R"(\w+|[^\w\s])");
+    auto words_begin = std::sregex_iterator(str.begin(), str.end(), word_regex);
+    auto words_end = std::sregex_iterator();
+    return std::distance(words_begin, words_end);
 }
 
 } // namespace core
